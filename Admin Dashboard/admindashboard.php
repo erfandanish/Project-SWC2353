74d083b5efc91cb45ab1d1309d05427cd29d4071
erfandanish <151341506+erfandanish@users.noreply.github.com>
2024-11-19 13:59:03 +0800

    Create admindashboard.php

---
+++ Admin Dashboard/admindashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+<link href="admin_dashboard.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="login">
+		
+	<a href="login.php"><img src="Images/login-computer-icons-download-avatar-icon-thumbnail.jpg" width="40" height="40" alt="login"></a>
+		
+	 </div>	
+ 
+<div class="dashboard">
+  <h1>Admin Dashboard</h1>
+        <div class="actions">
+            <a href="view_products.php">View Products</a>
+            <a href="add_product.php">Add Product</a>
+            <a href="delete_product.php">Delete Product</a>
+			<a href="submissions.php">View Feedback</a>
+			
+        </div>
+    </div>
+
+
+</body>
+</html>
